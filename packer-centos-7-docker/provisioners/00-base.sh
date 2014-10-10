0cfe5700e5fbb1777a06ac13f5a2451f8be0cd98
Russ McKendrick <russ@media-glass.es>
2014-10-10 14:40:13 +0100

    Adding some extra packages

--- packer-centos-7-docker/provisioners/00-base.sh
+++ packer-centos-7-docker/provisioners/00-base.sh
@@ -2,7 +2,7 @@
 
 # update
 yum update -y
-yum install wget -y
+yum install wget git vim-enhanced -y
 
 # update root certs
 wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
