27816f5da319b3e748fb86eb90df5073f299e531
Russ McKendrick <russ@media-glass.es>
2014-10-20 17:16:18 +0100

    Updating FIG Version

--- packer-centos-7-docker/provisioners/10-docker.sh
+++ packer-centos-7-docker/provisioners/10-docker.sh
@@ -30,8 +30,7 @@ nsenter -V
 docker rmi jpetazzo/nsenter
 
 # Install Fig
-curl -L https://github.com/docker/fig/releases/download/0.5.2/linux > /usr/local/bin/fig
-chmod 755 /usr/local/bin/fig
+curl -L https://github.com/docker/fig/releases/download/1.0.0/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
 /usr/local/bin/fig --version
 
 # Pull down the NGINX Route imgage
