5ce5c38b009616c5e62db953311b670a3379cafe
Danny Roberts <danny.roberts@reconnix.com>
2015-04-30 16:30:47 +0100

    Switched outvbox builder for openstack/kvm. Added ovf post processor for VMware builds. Used new kickstart scripts. Switched to use open-vm-tools instead of building propriatory extensions for vmware images. Populated README

--- CentOS7/scripts/base.sh
+++ CentOS7/scripts/base.sh
@@ -2,10 +2,7 @@
 
 # Install some basics and update
 
-yum install -y wget nfs-utils rpcbind git vim-enhanced vim-common net-tools
-systemctl enable rpcbind
-systemctl enable nfs-lock
-systemctl enable nfs-idmap
+yum install -y epel-release wget
 
 # add 'single-request-reopen' so it is included when /etc/resolv.conf is generated
 echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
